91e042025d99f1360af4e0a2206b33dbbc498cc0
Hikaari <32917606+Hikaari@users.noreply.github.com>
2023-08-25 23:04:28 +0400

    Update entrypoint.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
-python manage.py flush --no-input
+# Примените миграции
 python manage.py migrate
 
-exec "$@"
+# Создайте суперпользователя (администратора) Django
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
+
+# Запустите Gunicorn для Django
+gunicorn project.wsgi:application --bind 0.0.0.0:8000
